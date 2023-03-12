bab1e5b6aa237904861ef99d4c25ab824b58f13b
VleronaM <vm58482@ubt-uni.net>
2023-03-12 21:34:54 +0100

    Revert "Vlerona's changes"
    This reverts commit 233604d73c95cb85ee25ff748f70a1805d8c116d.

--- delete-about.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../controllers/aboutController.php';
-if (isset($_GET['id'])){
-    $aboutID= $_GET['id'];
-}
-$about=new AboutController;
-$about->delete($aboutID);
-
-?>
--- delete-blogs.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../controllers/BlogController.php';
-if (isset($_GET['id'])){
-    $blogID= $_GET['id'];
-}
-$blog=new BlogController;
-$blog->delete($blogID);
-
-?>
