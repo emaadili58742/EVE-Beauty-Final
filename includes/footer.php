c310769a7721bdec581b64783397863525fe0c05
eljesa162 <eljesakrass7@gmail.com>
2023-03-12 21:42:48 +0100

    eljesa's commit

---
+++ includes/footer.php
@@ -0,0 +1,52 @@
+<section class="footer">
+
+<div class="box-container">
+
+    <div class="box">
+        <h3>quick links</h3>
+        <a class="link" href="#home"> <i class="fas fa-angle-right"></i> Home</a>
+        <a class="link" href="#about"> <i class="fas fa-angle-right"></i> Rreth Nesh</a>
+        <a class="link" href="#shop"> <i class="fas fa-angle-right"></i> Dyqani</a>
+        <a class="link" href="#message"> <i class="fas fa-angle-right"></i> Na kontaktoni</a>
+    </div>
+
+    <div class="box">
+            <h3>Links te shpejte</h3>
+            <a href="index.php#home" class="link"> <i class="fas fa-arrow-right"></i> home </a>
+            <a href="index.php#features" class="link"> <i class="fas fa-arrow-right"></i> features </a>
+            <a href="products.php" class="link"> <i class="fas fa-arrow-right"></i> products </a>
+            <a href="index.php#categories" class="link"> <i class="fas fa-arrow-right"></i> categories </a>
+            <a href="blog1.php" class="link"> <i class="fas fa-arrow-right"></i> blogs </a>
+        </div>
+
+    <div class="box">
+    <h3>Informacionet e kontaktit</h3>
+    <a class="link" href="#"><i class="fas fa-phone"></i>+383-45-867-338</a>
+    <a class="link" href="#"><i class="fas fa-phone"></i>+383-45-867-338</a>
+    <a class="link" href="#"><i class="fas fa-envelope"></i>xy212222@ubt-uni.net</a>
+    <a class="link" href="#"><i class="fas fa-map"></i>Prishtina, Kosovo </a>
+    <div class="share">
+        <a href="#" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-instagram"></a>
+        <a href="#" class="fab fa-linkedin"></a>
+    </div>
+</div>
+
+<div class="box">
+    <h3>newsletter</h3>
+    <p>Vendosni email adresen tuaj per te marr njoftimet e fundit<p>
+    <form action="">
+        <input type="email" name="" placeholder="email adresa" id="" class="email">
+        <input type="submit" value="subscribe" id="subscribe" class="btn">
+    </form>
+</div>
+
+</div>
+
+<div class="credit">Krijuar nga <span>E</span>ma Adili, <span>V</span>lerona Murtezi dhe <span>E</span>ljesa Krasniqi</div>
+</section>
+
+
+
+</section>
